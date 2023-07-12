d803d750ce4b0c0e4e9ff4f0272bf8b5e1e0eb60
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-07-12 17:21:48 -0300

    atts

--- mobile/LoginRegistro/Login/MainPage.xaml.cs
+++ mobile/LoginRegistro/Login/MainPage.xaml.cs
@@ -1,24 +1,18 @@
-﻿namespace Login;
+﻿
+namespace Login;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void TogglePasswordVisibility(object sender, EventArgs e)
+    {
+        passwordEntry.IsPassword = !passwordEntry.IsPassword;
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
